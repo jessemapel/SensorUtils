0fe02a25c0022251618e48a584e7aa7c97e4b9aa
Kristin <kberry@usgs.gov>
2018-08-17 10:24:09 -0700

    Phase angle and illuminatorPosition updated with SensorModelFactory shell for testing (#143)
    * Add pi and other functions to SensorMath.cpp
    
    * Added a rough draft of phaseAngle and illuminatorPosition
    
    * Added placeholder SensorModel, SensorModelFactory, CSMSensorModel for testing
    
    * Fix typo and update tests for phaseAngle
    
    * Add test file for CSMSensorModel to keep test coverage at 100 percenet

--- include/sensormodel/SensorModel.h
+++ include/sensormodel/SensorModel.h
@@ -16,18 +16,14 @@ class SensorModel {
 *
 *
 */
-
-
-
 public:
 
   virtual CartesianPoint imageToGround(ImagePoint &) =0;
   virtual ImagePoint groundToImage(CartesianPoint &) = 0;
   virtual CartesianVector groundToLook(CartesianPoint & ) = 0;
   virtual double imageTime(ImagePoint & )= 0;
-
-
-
+  virtual CartesianPoint getSensorPosition(ImagePoint &) = 0; 
+  virtual CartesianVector getIlluminationDirection (CartesianPoint &) = 0; 
 };
 
 #endif
--- src/sensorcore/Sensor.cpp
+++ src/sensorcore/Sensor.cpp
@@ -8,9 +8,11 @@
 #include "SensorMath.h"
 #include "ShapeModel.h"
 #include "ShapeModelFactory.h"
+#include "SensorModelFactory.h"
 
 Sensor::Sensor(const std::string &metaData, const std::string &sensorName) {
   m_shapeModel = ShapeModelFactory::create("test");
+  m_sensorModel = SensorModelFactory::create("test"); 
 }
 
 
@@ -38,13 +40,90 @@ double Sensor::emissionAngle(const ImagePoint &imagePoint) {
 }
 
 
+/**
+   * Computes and returns phase angle, in radians, given the positions of the
+   * observer and illuminator.
+   *
+   * Phase Angle: The angle between the vector from the surface intersection point to
+   * the observer (usually the spacecraft) and the vector from the surface intersection
+   * point to the illuminator (usually the sun).
+   *
+   * @author Kaj Williams
+   *
+   * @param groundPoint The point on the body to calculate the phase angle at 
+   *  
+   * @return @b double Phase angle, in radians.
+   */
 double Sensor::phaseAngle(const CartesianPoint &groundPoint) {
-  return 0.0;
+  CartesianPoint input = groundPoint; 
+  ImagePoint imagePoint = m_sensorModel->groundToImage(input); 
+  CartesianPoint sensorPosition = m_sensorModel->getSensorPosition(imagePoint); 
+  CartesianPoint sunPosition = illuminatorPosition(imagePoint);
+
+  CartesianVector surfaceToObserver = sensormath::subtract(groundPoint, sensorPosition); 
+  CartesianVector surfaceToSun = sensormath::subtract(groundPoint, sunPosition); 
+  // why would we need to normalize? 
+
+  double cos_angle = sensormath::dot(surfaceToObserver, surfaceToSun);
+
+  if(cos_angle >= 1.0) return 0.0;
+  if(cos_angle <= -1.0) return M_PI;
+
+  return acos(cos_angle);
 }
 
 
+/**
+   * Computes and returns phase angle, in radians, given the positions of the
+   * observer and illuminator.
+   *
+   * Phase Angle: The angle between the vector from the surface intersection point to
+   * the observer (usually the spacecraft) and the vector from the surface intersection
+   * point to the illuminator (usually the sun).
+   *
+   * @author Kaj Williams 
+   *
+   * @param imagePoint The point on the image to calculate the phaseAngle at. 
+   *  
+   * @return @b double Phase angle, in radians.
+   */
 double Sensor::phaseAngle(const ImagePoint &imagePoint) {
-  return 0.0;
+  ImagePoint input = imagePoint; 
+  CartesianPoint groundPoint = m_sensorModel->imageToGround(input); 
+  CartesianPoint sensorPosition = m_sensorModel->getSensorPosition(input); 
+  CartesianPoint sunPosition = illuminatorPosition(input);
+
+  CartesianVector surfaceToObserver = sensormath::subtract(groundPoint, sensorPosition); 
+  CartesianVector surfaceToSun = sensormath::subtract(groundPoint, sunPosition); 
+  // why would we need to normalize? 
+
+  double cos_angle = sensormath::dot(surfaceToObserver, surfaceToSun);
+
+  if(cos_angle >= 1.0) return 0.0;
+  if(cos_angle <= -1.0) return M_PI;
+
+  return acos(cos_angle);
+}
+
+
+/**
+ * This method calculates the position of the illuminator.
+ *
+ * @author Jeannie Backer
+ *
+ * @param CartesianPoint imagePoint The point in the image to calculate the illuminator 
+ *                                  position at.
+ *        
+ * @return The position of the illuminator in body fixed coordinate system.
+ */
+CartesianPoint Sensor::illuminatorPosition(ImagePoint imagePoint) {
+  // sun pos (center body to center sun)
+  // is body fixed ground coordinated (center body to ground point)
+  // minus the illumination direction (center sun to ground point)
+  CartesianPoint groundPoint = m_sensorModel->imageToGround(imagePoint);
+  CartesianVector illuminatorDirection = m_sensorModel->getIlluminationDirection(groundPoint); 
+
+  return sensormath::subtract(illuminatorDirection, groundPoint);
 }
 
 
